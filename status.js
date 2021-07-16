768ea5306805d174a112f2a21700838195297204
aar.56aint <aaron.hays_7@yahoo.com>
2021-07-16 03:30:13 +0100

    increased time for http_req_duration

--- status.js
+++ status.js
@@ -6,7 +6,7 @@ export let options = {
   duration: "5s",
   thresholds: {
     http_req_failed: ["rate<0.01"], // http errors should be less than 1%
-    http_req_duration: ["p(95)<200"], // 95% of requests should be below 200ms
+    http_req_duration: ["p(95)<500"], // 95% of requests should be below 200ms
   },
 };
 
